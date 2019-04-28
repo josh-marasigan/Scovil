595ced3d78c014c49949f67d784c89851d13ca11
Josh Marasigan <marasigan.josh@utexas.edu>
2019-04-27 19:06:34 -0700

    Small animation for logo screen

--- Scovil/ViewController.swift
+++ Scovil/ViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import SnapKit
 
 // Note: - Replace this initial view with a loading/logo screen.
 class ViewController: UIViewController {
 
     // MARK: - Properties
+    private lazy var topLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 36)
+        label.textColor = UIColor.white
+        label.text = "It's"
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var bottomLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 48)
+        label.textColor = UIColor.white
+        label.text = "Lit"
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        return label
+    }()
     
     // MARK: - VC Lifecycle
     override func viewWillAppear(_ animated: Bool)
@@ -28,6 +48,15 @@ class ViewController: UIViewController {
             .setNavigationBarHidden(false, animated: animated)
     }
     
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(wallDeadline: .now() + 0.5) {
+            self.animateIn()
+        }
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -42,6 +71,21 @@ class ViewController: UIViewController {
     private func configUI()
     {
         self.setBackgroundColor()
+        self.resetAnimations()
+        
+        self.view.addSubview(self.topLabel)
+        self.topLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+                .offset(UIScreen.main.bounds.height / 4)
+            make.centerX.equalToSuperview()
+        }
+        
+        self.view.addSubview(self.bottomLabel)
+        self.bottomLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.topLabel.snp.bottom)
+            make.centerX.equalTo(self.topLabel)
+            make.bottom.lessThanOrEqualToSuperview()
+        }
     }
     
     private func setBackgroundColor()
@@ -50,8 +94,51 @@ class ViewController: UIViewController {
         gradientBackgroundColor.frame = self.view.bounds
         gradientBackgroundColor.colors = [AppTheme.shared.color.primary.cgColor,
                                           AppTheme.shared.color.primaryLight.cgColor]
+        
         gradientBackgroundColor.startPoint = CGPoint(x: 1, y: 0)
         gradientBackgroundColor.endPoint = CGPoint(x: 1, y: 1)
         self.view.layer.addSublayer(gradientBackgroundColor)
+    }
+    
+    // MARK: - Animation
+    private func resetAnimations()
+    {
+        self.topLabel.alpha = 0.0
+        self.bottomLabel.alpha = 0.0
+        self.topLabel.transform = CGAffineTransform(translationX: 0.0, y: -32.0)
+        self.bottomLabel.transform = CGAffineTransform(translationX: 0.0, y: -32.0)
+    }
+    
+    private func animateIn()
+    {
+        UIView.animate(
+            withDuration: 0.4,
+            delay: 0.0,
+            usingSpringWithDamping: 1.0,
+            initialSpringVelocity: 1.5,
+            options: .curveEaseIn,
+            animations:
+            { [weak self] in
+                guard let self = self else { return }
+                self.topLabel.alpha = 1.0
+                self.topLabel.transform = .identity
+                self.view.layoutIfNeeded()
+                
+        }) { _ in
+            
+            UIView.animate(
+                withDuration: 0.4,
+                delay: 0.0,
+                usingSpringWithDamping: 1.0,
+                initialSpringVelocity: 1.5,
+                options: .curveEaseIn,
+                animations:
+                { [weak self] in
+                    guard let self = self else { return }
+                    self.bottomLabel.alpha = 1.0
+                    self.bottomLabel.transform = .identity
+                    self.view.layoutIfNeeded()
+            })
+        }
     }
 }
