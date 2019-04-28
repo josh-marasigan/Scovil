205e66232b8f5b6350c00adcf8ad19187c7023f3
Josh Marasigan <marasigan.josh@utexas.edu>
2019-04-27 17:46:31 -0700

    Initial pods

---
+++ Podfile
@@ -0,0 +1,6 @@
+ platform :ios, '10.0'
+
+target 'Scovil' do
+  use_frameworks!
+  pod 'SnapKit', '4.2.0'
+end
---
+++ Pods/Target Support Files/Pods-Scovil/Pods-Scovil-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Scovil : NSObject
+@end
+@implementation PodsDummy_Pods_Scovil
+@end
